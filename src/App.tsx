ce72430a1dca526c2b78645cf507667bb4e8e20c
Sam Sudekum <shsudekum@gmail.com>
2021-12-05 20:28:58 -0600

    remove under contstruction banner

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import Playmat from './component/Playmat/Playmat';
 import { CustomDragLayer } from './component/CustomDrag/CustomDragLayer';
 import SettingsButton from './component/Settings/SettingsButton';
-import Banner from './component/Banner/Banner';
 import { useDispatch } from 'react-redux';
 import { hideContextMenus, setIsDragging } from './redux/actions';
 
@@ -31,7 +30,6 @@ const App: FC = () => {
       </DndProvider>
 
       <Logo />
-      <Banner>Under Construction</Banner>
       <SettingsButton />
     </div>
   );
